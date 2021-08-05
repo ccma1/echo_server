56245efd55f85cc4b9f558c63a2bcecca33248fb
Cheng Ma <ccma@andrew.cmu.edu>
2021-08-05 15:02:52 -0500

    buggy first server and client

---
+++ client.c
@@ -0,0 +1,93 @@
+/**
+ * echo client
+ */
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <error.h>
+#include <errno.h>
+
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+/**constants */
+#define MAXLINE 8194
+
+//opens connection to server at (hostname, port) and returns socket descriptor
+int open_clientfd(char *hostname, char *port);
+
+int main(int argc, char **argv) 
+{
+    int clientfd;
+    char *host, *port, buf[MAXLINE];
+    int n;
+
+    if (argc != 3) {
+        fprintf(stderr, "usage: %s <host> <port>\n", argv[0]);
+        exit(0);
+    }
+    host = argv[1];
+    port = argv[2];
+
+    clientfd = open_clientfd(host, port);
+    memset(buf, 0, MAXLINE);
+
+    while (fgets(buf, MAXLINE, stdin) != NULL) {
+        if ((n = write(clientfd, buf, strlen(buf))) < 0) {
+            fprintf(stderr, "ERROR writing to socket %s\n",strerror(errno));
+        }
+        printf("Sent to server %s\n", buf);
+        //memset(buf, 0, MAXLINE);
+        if ((n = read(clientfd, buf, strlen(buf))) < 0) {
+            fprintf(stderr, "ERROR writing to socket %s\n",strerror(errno));
+        }
+        printf("Echo from server: %s\n", buf);
+    }
+    close(clientfd); //line:netp:echoclient:close
+    exit(0);
+}
+
+/*
+ * open_clientfd - Open connection to server at <hostname, port> and
+ *     return a socket descriptor ready for reading and writing. This
+ *     function is reentrant and protocol-independent.
+*/
+int open_clientfd(char *hostname, char *port) {
+    int clientfd, rc;
+    struct addrinfo hints, *listp, *p;
+
+    /* Get a list of potential server addresses */
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_socktype = SOCK_STREAM;  /* Open a connection */
+    hints.ai_flags = AI_NUMERICSERV;  /* ... using a numeric port arg. */
+    hints.ai_flags |= AI_ADDRCONFIG;  /* Recommended for connections */
+    if ((rc = getaddrinfo(hostname, port, &hints, &listp)) != 0) {
+        fprintf(stderr, "getaddrinfo failed (%s:%s): %s\n", hostname, port, gai_strerror(rc));
+        return -2;
+    }
+  
+    /* Walk the list for one that we can successfully connect to */
+    for (p = listp; p; p = p->ai_next) {
+        /* Create a socket descriptor */
+        if ((clientfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) < 0) 
+            continue; /* Socket failed, try the next */
+
+        /* Connect to the server */
+        if (connect(clientfd, p->ai_addr, p->ai_addrlen) != -1) 
+            break; /* Success */
+        if (close(clientfd) < 0) { /* Connect failed, try another */  //line:netp:openclientfd:closefd
+            fprintf(stderr, "open_clientfd: close failed: %s\n", strerror(errno));
+            return -1;
+        } 
+    } 
+
+    /* Clean up */
+    freeaddrinfo(listp);
+    if (!p) /* All connects failed */
+        return -1;
+    else    /* The last connect succeeded */
+        return clientfd;
+}
---
+++ echo_server_blocking.c
@@ -0,0 +1,114 @@
+/**
+ * a simple blocking echo server
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <error.h>
+#include <errno.h>
+
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+/*constants */
+#define MAXLINE 8194
+#define LISTENQ 1024
+
+//sockaddr def
+typedef struct sockaddr SA; 
+
+//echo 
+void echo(int connfd);
+//returns a listening socket ready to receive conn requests on port
+int open_listenfd(char *port);
+
+int main(int argc, char **argv){
+    int listenfd, connfd; //listening and connection file descriptor
+    socklen_t clientlen; //specifies the size of client's addr
+    struct sockaddr_storage clientaddr; //socket addr of client
+    char client_hostname[MAXLINE], client_port[MAXLINE];
+    int rc;
+
+    listenfd = open_listenfd(argv[1]);
+    while(1) {
+        clientlen = sizeof(struct sockaddr_storage);
+        //wait for connection request from client on listenfd
+        if ((connfd = accept(listenfd, (SA*)&clientaddr, &clientlen) < 0)) {
+            fprintf(stderr, "accept error: %s\n", strerror(errno));
+            exit(-1);
+        }
+        if ((rc = getnameinfo((SA*)&clientaddr, clientlen,client_hostname, MAXLINE, client_port, MAXLINE, 0)) < 0) {
+                        fprintf(stderr, "getnameinfo error: %s\n", gai_strerror(rc));
+        }
+        printf("Connected to (%s, %s)\n", client_hostname, client_port);
+        echo(connfd);
+        close(connfd);
+    }
+    exit(0);
+}
+
+/**
+ * reads and echoes text from client until client closes connection
+ */
+void echo(int connfd) {
+    size_t n;
+    char buf[MAXLINE];
+    
+    while ((n = read(connfd, buf, MAXLINE)) != 0) {
+        printf("server received %d bytes \n", (int)n);
+        write(connfd, buf, n);
+    }
+    close(connfd);
+}
+
+/**
+ * opens and returns a listening socket on port
+ */
+int open_listenfd(char *port) {
+    struct addrinfo hints, *listp, *p;
+    int listenfd, rc, optval=1;
+
+    //gets linked list of addrinfo structs for host and service
+    memset(&hints, 0, sizeof(struct addrinfo));
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE | AI_ADDRCONFIG;
+    hints.ai_flags |= AI_NUMERICSERV;
+    if ((rc = getaddrinfo(NULL, port, &hints, &listp)) != 0) {
+        fprintf(stderr, "getaddrinfo fails port %s: %s\n", port, gai_strerror(rc));
+        return -2;
+    }
+
+    //walk list for a socket that can be binded to
+    for (p = listp; p; p = p->ai_next) {
+        //create socket descriptor
+        if ((listenfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) < 0)
+            continue; //socket failed and try
+        
+        /* Eliminates "Address already in use" error from bind */
+        setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR,    //line:netp:csapp:setsockopt
+                   (const void *)&optval , sizeof(int));
+        
+        //bind the descriptor to the address
+        if (bind(listenfd, p->ai_addr, p->ai_addrlen) == 0)
+            break;
+        if (close(listenfd) < 0) {
+            fprintf(stderr, "open_listenfd close failed: %s\n", strerror(errno));
+            return -1;
+        }
+    }
+
+    //clean up getaddrinfo
+    freeaddrinfo(listp);
+    //if no address worked
+    if (!p) return -1;
+
+    //make p a listening socket ready to accept connection requests
+    if (listen(listenfd, LISTENQ) < 0) {
+        fprintf(stderr, "listen error: %s\n", strerror(errno));
+        return -1;
+    }
+    return listenfd;
+}   
